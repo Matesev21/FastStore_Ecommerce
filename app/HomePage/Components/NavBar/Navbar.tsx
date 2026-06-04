2490b9e7c0ac7be0c4891e25e816b7eb7cf1a709
Matesev21 <mateof.sevilla@gmail.com>
2026-06-04 13:32:27 -0400

    Navbar hecho y en progreso el mock data

---
+++ app/HomePage/Components/NavBar/Navbar.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import Link from "next/link";
+import Logo from "@/app/HomePage/Assets/LogoFastStore.png";
+import styles from "./Navbar.module.css";
+
+export const NavBar = () => {
+  return (
+    <header className={styles.navbar}>
+      <div className={styles.logoContainer}>
+        <Link href="/">
+          <Image
+            src={Logo}
+            alt="FastStore Logo"
+            className={styles.logo}
+            width={130}
+            height={130}
+            priority
+            style={{ objectFit: "contain" }}
+          />
+        </Link>
+      </div>
+
+      <nav className={styles.navLinks}>
+        <Link href="/" className={styles.link}>
+          Inicio
+        </Link>
+        <Link href="/categorias" className={styles.link}>
+          Categorías
+        </Link>
+        <Link href="/contacto" className={styles.link}>
+          Contacto
+        </Link>
+      </nav>
+
+      <div className={styles.actionContainer}>
+        <Link href="/tienda" className={styles.ctaButton}>
+          Tienda Online
+        </Link>
+      </div>
+    </header>
+  );
+};
---
+++ app/HomePage/HomePage.tsx
@@ -0,0 +1,9 @@
+import { NavBar } from "./Components/NavBar/Navbar";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+};
